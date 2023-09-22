ddf7294100a99346a3d38fa56a25bb51cd470856
Shreyaskar <shreyash0790@gmail.com>
2023-09-22 21:08:11 +0530

    extrass

--- Routes/group-admin.js
+++ Routes/group-admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const GAdmincontroller=require('../controllers/GroupAdmin');
+const GAdmincontroller=require('../controllers/group-admin');
 const UserAuthen=require('../middlewares/autherizations');
 
 
--- Routes/sign-up.js
+++ Routes/sign-up.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Signcontroller=require('../controllers/SignUp');
+const Signcontroller=require('../controllers/signUp');
 
 router.post('/SignUp', Signcontroller.AddUser);
 
